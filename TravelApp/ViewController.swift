c094ef661e235fdef0f7bf34fdb9febe634535cf
EmmanuelC1 <emcastillo@csumb.edu>
2020-03-09 16:48:44 -0700

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Emmanuel Castillo on 3/9/20.
+//  Copyright © 2020 Emmanuel Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
