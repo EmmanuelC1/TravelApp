df216e759818b91e3e6713a0044bb1d600632c52
EmmanuelC1 <emcastillo@csumb.edu>
2020-04-12 21:30:36 -0700

    Logo added, users can now stay logged in or log out

--- TravelApp/SceneDelegate.swift
+++ TravelApp/SceneDelegate.swift
@@ -19,6 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil {
+                   let main = UIStoryboard(name: "Main", bundle: nil)
+                   let HomeNavigationController = main.instantiateViewController(withIdentifier: "HomeNavigationController")
+                   window?.rootViewController = HomeNavigationController
+               }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
