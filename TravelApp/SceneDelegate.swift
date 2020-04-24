3c8ad6561c5548c499edb1db472b6ab724564a9e
EmmanuelC1 <emcastillo@csumb.edu>
2020-04-23 19:02:32 -0700

    removed feature that lets user stayed logged in (causing issues)

--- TravelApp/SceneDelegate.swift
+++ TravelApp/SceneDelegate.swift
@@ -20,11 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        if PFUser.current() != nil {
-                   let main = UIStoryboard(name: "Main", bundle: nil)
-                   let HomeNavigationController = main.instantiateViewController(withIdentifier: "HomeNavigationController")
-                   window?.rootViewController = HomeNavigationController
-               }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
